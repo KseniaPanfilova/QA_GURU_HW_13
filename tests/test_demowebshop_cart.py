11ed519b5e5f5a628ab2eeb88e798132c1f04c83
Ksenia Panfilova <kseniapanfilova@gmail.com>
2024-09-25 17:09:02 +0300

    update tests

--- tests/test_demowebshop_cart.py
+++ tests/test_demowebshop_cart.py
@@ -21,6 +21,32 @@ def auth_cookie():
     return auth
 
 
+def add_product_to_cart():
+    response = requests.post(
+        url=base_url + '/addproducttocart/catalog/43/1/1',
+        cookies=auth_cookie()
+    )
+    attach(body=response.request.url, name='Request', attachment_type=AttachmentType.TEXT,
+           extension="txt")
+    attach(body=json.dumps(dict(response.request.headers),
+                           indent=4, ensure_ascii=True), name='Request Headers',
+           attachment_type=AttachmentType.JSON,
+           extension="json")
+    attach(body=json.dumps(response.json(),
+                           indent=4, ensure_ascii=True), name="Response", attachment_type=AttachmentType.JSON,
+           extension="json")
+    attach(body=str(response.cookies.get('Nop.customer')), name="Cookies", attachment_type=AttachmentType.TEXT,
+           extension="txt")
+    logging.info(response.request.url)
+    logging.info(response.status_code)
+    logging.info(response.text)
+
+
+def remove_product_from_cart():
+    browser.element('[name="removefromcart"]').click()
+    browser.element('[name="updatecart"]').click()
+
+
 def test_login():
     with step('Set cookie from API'):
         browser.open(base_url)
@@ -38,24 +64,7 @@ def test_login():
 
 def test_add_product_to_cart():
     with step('Add product to cart by API'):
-        response = requests.post(
-            url=base_url + '/addproducttocart/catalog/43/1/1',
-            cookies=auth_cookie()
-        )
-        attach(body=response.request.url, name='Request', attachment_type=AttachmentType.TEXT,
-               extension="txt")
-        attach(body=json.dumps(dict(response.request.headers),
-                               indent=4, ensure_ascii=True), name='Request Headers',
-               attachment_type=AttachmentType.JSON,
-               extension="json")
-        attach(body=json.dumps(response.json(),
-                               indent=4, ensure_ascii=True), name="Response", attachment_type=AttachmentType.JSON,
-               extension="json")
-        attach(body=str(response.cookies.get('Nop.customer')), name="Cookies", attachment_type=AttachmentType.TEXT,
-               extension="txt")
-        logging.info(response.request.url)
-        logging.info(response.status_code)
-        logging.info(response.text)
+        add_product_to_cart()
 
     with step('Set cookies from API'):
         browser.open(base_url + '/cart')
@@ -68,11 +77,17 @@ def test_add_product_to_cart():
         browser.element('.cart').element('.product').element('[href="/smartphone"]').should(be.visible)
         browser.element('.qty-input').should(have.attribute('value', '1'))
 
+    with step('Clear cart'):
+        remove_product_from_cart()
+
     with step('Close browser'):
         browser.quit()
 
 
 def test_remove_product_from_cart():
+    with step('Add product to cart'):
+        add_product_to_cart()
+
     with step('Set cookies from API'):
         browser.open(base_url + '/cart')
         browser.driver.add_cookie({'name': 'NOPCOMMERCE.AUTH', 'value': auth_cookie().get('NOPCOMMERCE.AUTH')})
@@ -80,11 +95,8 @@ def test_remove_product_from_cart():
     with step('Open cart page'):
         browser.open(base_url + '/cart')
 
-    with step('Check Remove check-box'):
-        browser.element('[name="removefromcart"]').click()
-
     with step('Remove product from cart'):
-        browser.element('[name="updatecart"]').click()
+        remove_product_from_cart()
 
     with step('Verify removing of product from cart'):
         browser.element('.order-summary-content').should(have.exact_text('Your Shopping Cart is empty!'))
